3898389a96498bfb20edb4c071cba878423d7bae
Carter Fitzgerald <carterjf2003@icloud.com>
2026-02-28 18:00:14 +1100

    Create shared contracts project for cross-service event definitions

--- FraudDetectionWorker/Program.cs
+++ FraudDetectionWorker/Program.cs
@@ -1,4 +1,5 @@
 using FraudDetectionWorker;
+using FraudDetectionWorker.Application;
 using FraudDetectionWorker.Data;
 using FraudDetectionWorker.Messaging;
 using Microsoft.EntityFrameworkCore;
@@ -7,12 +8,14 @@ var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.Configure<RabbitMqOptions>(builder.Configuration.GetSection("RabbitMq"));
 
-// ✅ Register factory because Worker injects IDbContextFactory<TransactionDbContext>
 builder.Services.AddDbContextFactory<TransactionDbContext>(options =>
 {
     var cs = builder.Configuration.GetConnectionString("DefaultConnection");
     options.UseSqlServer(cs);
 });
+
+builder.Services.AddSingleton<IMessageConsumer, RabbitMqMessageConsumer>();
+builder.Services.AddSingleton<TransactionCreatedHandler>();
 
 builder.Services.AddHostedService<Worker>();
 
